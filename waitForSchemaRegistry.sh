5c809c6f71234d88378b6e68f032346431f06da8
joangui <joan@ac.upc.edu>
2019-03-19 15:31:04 +0100

    Modified to accept SCHEMA_REGISTRY_PORT, default 8081

--- waitForSchemaRegistry.sh
+++ waitForSchemaRegistry.sh
@@ -2,7 +2,12 @@
 
 echo "Waiting for Schema-registry"
 
-export URL="$SCHEMA_REGISTRY_HOST_NAME:8081"
+if [ -z ${SCHEMA_REGISTRY_PORT} ]
+then
+    SCHEMA_REGISTRY_PORT = 8081
+fi
+
+export URL="$SCHEMA_REGISTRY_HOST_NAME:$SCHEMA_REGISTRY_PORT"
 
 
 
