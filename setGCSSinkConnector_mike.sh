4a8696a49f5b86daacceaf585c8b7dcc1e79e84c
joangui <joan@ac.upc.edu>
2019-10-22 14:04:50 +0200

    adding hotplug

---
+++ setGCSSinkConnector_mike.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#git clone https://github.com/My-TRAC/ConfigurationScripts.git
+#./ConfigurationScripts/waitForKafkaConenct.sh
+
+
+echo $CONNECT_BOOTSTRAP_SERVERS
+echo $TOPICS
+echo $KAFKA_CONNECT_HOST
+
+
+curl -X POST -H "Content-Type: application/json" --data "{ \"name\": \"gcs\", \"config\": { \"connector.class\": \"io.confluent.connect.gcs.GcsSinkConnector\", \"tasks.max\": \"1\", \"topics\": \"$TOPICS\", \"gcs.bucket.name\": \"my-trac_storage\", \"gcs.part.size\": \"5242880\", \"flush.size\": \"3\", \"gcs.credentials.path\": \"/opt/cigo-core/credentials2/My-TRAC-9b31e91cc907.json\", \"storage.class\": \"io.confluent.connect.gcs.storage.GcsStorage\", \"format.class\": \"io.confluent.connect.gcs.format.avro.AvroFormat\", \"partitioner.class\": \"io.confluent.connect.storage.partitioner.DefaultPartitioner\", \"schema.compatibility\": \"NONE\", \"confluent.topic.bootstrap.servers\": \"$CONNECT_BOOTSTRAP_SERVERS\", \"confluent.topic.replication.factor\": \"1\", \"name\": \"gcs\" }}" $KAFKA_CONNECT_HOST:28083/connectors
